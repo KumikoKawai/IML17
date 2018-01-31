93c3f41f268e1af79a232640d916e21f917b144a
MAST-Hiroki-Yamaguchi <pmdm9n4b0603@i.softbank.jp>
2018-01-31 15:08:35 +0900

    1/31 ４限終了時点のソースコード

--- Assets/makeLine.cs
+++ Assets/makeLine.cs
@@ -2,8 +2,31 @@ using UnityEngine;
 using System.Collections;
 
 public class makeLine : MonoBehaviour {
+
+  LineAnimation line;
+  GameObject Saisyo;
+  GameObject Kokohe;
+  GameObject LineRenderer_prefab;
+
  void Start () {
+
+  // LineRenderer（親オブジェクト）を生成
   GameObject prefab = (GameObject)Resources.Load("LineRenderer");
-  Instantiate(prefab,transform.position,transform.rotation);
+  Instantiate(prefab);
+  LineRenderer_prefab = GameObject.Find("LineRenderer(Clone)/LineRenderer Object");
+
+  Debug.Log(LineRenderer_prefab);
+
+  // lineにLineAnimationスクリプトのコンポーネントを代入
+  Saisyo = GameObject.Find("Start1");
+  Kokohe = GameObject.Find("End1");
+
+  line = LineRenderer_prefab.GetComponent<LineAnimation>();
+
+  // LineAnimationのInspectorに代入
+  line.saisyo = Saisyo.transform;
+  line.kokohe = Kokohe.transform;
+
+
  }
 }
